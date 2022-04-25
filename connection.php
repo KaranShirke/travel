48aeea041717fb5a62aba23986273d5d38d19095
Karan Shirke <karanshirke888@gmail.com>
2022-04-24 21:33:23 -0700

    first commit

---
+++ connection.php
@@ -0,0 +1,7 @@
+<?php  
+     	$host = "127.0.0.1";
+		 $username = "root";
+		 $pass = "foobar";  
+       	$con = mysqli_connect ($host , $username , "", 'travel') or die("unable to connect to host");  
+		
+?>
